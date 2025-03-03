32583f8d1f8823233cd109bed531028b8117288f
KiranManaware <kiranmanaware123@gamil.com>
2025-03-03 11:05:14 +0530

    comments update

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -3,10 +3,24 @@ const { getUsers, getComplaints, updateComplaint, getComments, addComment } = re
 const adminProtect = require('../middlewares/adminMiddleware');
 const router=express.Router();
 
+//All user
+// /api/admin/users
 router.get('/users',adminProtect,getUsers);
+
+//All complaints
+// /api/admin/complaints
 router.get('/complaints',adminProtect,getComplaints);
+
+//All comments
+// /api/admin/comments
 router.get('/comments',adminProtect,getComments);
+
+//add comment
+// /api/admin/:id
 router.post('/:cid',adminProtect,addComment);
+
+//update complaint
+// /api/admin/:id
 router.put('/:cid',adminProtect,updateComplaint);
 
 module.exports=router;
